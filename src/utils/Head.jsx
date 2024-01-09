ce237debbeb315e1345b54308d24995d2945b7b8
Christian Larrea <crlarrea@protonmail.com>
2024-01-09 23:28:46 +0000

    move GTM deployment from Rudderstack to source code

--- src/utils/Head.jsx
+++ src/utils/Head.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
 import { Rudderstack } from "./Rudderstack";
+import { GTM } from "./GTM";
 export const Head = () => {
   return (
     <Helmet>
       <title>Christian Larrea</title>
       <script>{Rudderstack.toString()}</script>
       <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/ffb7659c9a8230d7c509cd70/script.js"></script>
+      <script>{GTM.toString()}</script>
     </Helmet>
   );
 };
